6c74980335d81855b75a5f0408634606a4c5c30b
Gabriel <gabrielcostatoledo@outlook.com>
2025-07-14 10:49:57 -0300

    feat: add initial home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,31 @@
+import { useState } from "react";
+import Home from "./components/home";
+import FirstStep from "./components/first-step";
+
+export type FormValues = {
+  type?: "frontend" | "backend" | "fullstack";
+};
+
 const App = () => {
+  const [currentStep, setCurrentStep] = useState(0);
+  const [data, setData] = useState<FormValues>({});
+
+  const MIN_STEP = 0;
+  const MAX_STEP = 3;
+
+  const next = (newData: Partial<FormValues>) => {
+    setData((data) => ({ ...data, ...newData }));
+
+    setCurrentStep((prev) => Math.min(prev + 1, MAX_STEP));
+  };
+
+  const back = () => setCurrentStep((prev) => Math.max(prev - 1, MIN_STEP));
+
   return (
-    <div>
-      <h1>Home</h1>
+    <div className="flex h-screen w-full flex-col items-center justify-center p-4">
+      {currentStep === 0 && <Home setCurrentStep={setCurrentStep} />}
+
+      {currentStep === 1 && <FirstStep onNext={next} defaultValues={data} />}
     </div>
   );
 };
---
+++ src/components/home.tsx
@@ -0,0 +1,29 @@
+import { Button } from "./ui/button";
+
+interface HomeProps {
+  setCurrentStep: (step: number) => void;
+}
+
+const Home = ({ setCurrentStep }: HomeProps) => {
+  return (
+    <div className="w-full max-w-2xl text-center">
+      <h1 className="mb-3 text-3xl font-bold">Project Pilot</h1>
+
+      <p className="text-sm">
+        Quer iniciar um novo projeto mas não tem ideia do que fazer? Deixe com a
+        gente.
+      </p>
+
+      <p className="text-sm">
+        Gaste menos tempo criando ideias e escopos de projetos e use esse tempo
+        codando.
+      </p>
+
+      <Button className="mt-5" onClick={() => setCurrentStep(1)}>
+        Começar
+      </Button>
+    </div>
+  );
+};
+
+export default Home;
